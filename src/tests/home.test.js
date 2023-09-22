c027e9d6ee21a540ac2a66fa084b6bf61087ae0e
gdvl777 <gdvl777@users.noreply.github.com>
2023-09-22 08:46:15 -0500

    Update home.test.js

--- src/tests/home.test.js
+++ src/tests/home.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import Home from '../components/Home.js';
+import Home from '../components/Home';
 
 test('renders the Home component', () => {
   // Render the Home component
