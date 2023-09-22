f206bd2e145820b442ac96a9923c2de15606dc8b
gdvl777 <gdvl777@users.noreply.github.com>
2023-09-22 09:32:45 -0500

    Update quote.test.js

--- src/tests/quote.test.js
+++ src/tests/quote.test.js
@@ -4,7 +4,7 @@ import Quote from '../components/Quote';
 
 // Mock the fetch function to simulate a successful response
 beforeAll(() => {
-  global.fetch = jest.fn(() => Promise.resolve({ ok: true, json: () => Promise.resolve([{ quote: 'Test Quote' }]), }));
+  global.fetch = jest.fn(() => Promise.resolve({ ok: true, json: () => Promise.resolve([{ quote: 'Test Quote' }]) }));
 });
 
 afterAll(() => {
