5f78e3e639b05fbda50e9534b1deb8d6466a0674
gdvl777 <gdvl777@users.noreply.github.com>
2023-09-22 08:38:23 -0500

    Create quote.test.js

---
+++ src/tests/quote.test.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { render, waitFor } from '@testing-library/react';
+import Quote from '../tests/Quote';
+
+// Mock the fetch function to simulate a successful response
+beforeAll(() => {
+  global.fetch = jest.fn(() =>
+    Promise.resolve({
+      ok: true,
+      json: () => Promise.resolve([{ quote: 'Test Quote' }]),
+    })
+  );
+});
+
+afterAll(() => {
+  global.fetch.mockClear();
+  delete global.fetch;
+});
+
+test('renders the Quote component', async () => {
+  // Render the Quote component
+  const { getByText, getByTestId } = render(<Quote />);
+
+  // Wait for the component to fetch data and render
+  await waitFor(() => {
+    expect(global.fetch).toHaveBeenCalled();
+  });
+
+  // Check if the loading text is not displayed
+  expect(getByText('Loading...')).not.toBeInTheDocument();
+
+  // Check if the quote is displayed
+  const quoteElement = getByText('Test Quote');
+  expect(quoteElement).toBeInTheDocument();
+});
