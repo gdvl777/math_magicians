03c662de558d1c0147b44e3c2f537f58ae36af5d
Braingenius2 <braingenius2@gmail.com>
2023-09-22 21:24:38 +0300

    Fix eslint error. Unused declaration

--- src/tests/quote.test.js
+++ src/tests/quote.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, waitFor } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import Quote from '../components/Quote';
 
 describe('Quote', () => {
