76c1c6e93e6db844ae5ed3880a579787911691b1
Braingenius2 <braingenius2@gmail.com>
2023-09-22 21:13:49 +0300

    Simplify tests and use Snapshots

--- src/tests/quote.test.js
+++ src/tests/quote.test.js
@@ -2,29 +2,14 @@ import React from 'react';
 import { render, waitFor } from '@testing-library/react';
 import Quote from '../components/Quote';
 
-// Mock the fetch function to simulate a successful response
-beforeAll(() => {
-  global.fetch = jest.fn(() => Promise.resolve({ ok: true, json: () => Promise.resolve([{ quote: 'Test Quote' }]) }));
-});
-
-afterAll(() => {
-  global.fetch.mockClear();
-  delete global.fetch;
-});
-
-test('renders the Quote component', async () => {
-  // Render the Quote component
-  const { getByText } = render(<Quote />);
-
-  // Wait for the component to fetch data and render
-  await waitFor(() => {
-    expect(global.fetch).toHaveBeenCalled();
+describe('Quote', () => {
+  it('renders the default quote', () => {
+    const quote = {
+      quote: 'The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart.',
+      author: 'Helen Keller',
+      category: 'Inspirational',
+    };
+    const { container } = render(<Quote quote={quote} />);
+    expect(container).toMatchSnapshot();
   });
-
-  // Check if the loading text is not displayed
-  expect(getByText('Loading...')).not.toBeInTheDocument();
-
-  // Check if the quote is displayed
-  const quoteElement = getByText('Test Quote');
-  expect(quoteElement).toBeInTheDocument();
 });
